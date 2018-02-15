08c327deaddf1728d7e3a7cd8b9290a93e24fa37
petrelli08 <xiuqi08@gmail.com>
2018-02-15 16:26:59 +0900

    Try to refresh page, but failed

--- main.py
+++ main.py
@@ -1,20 +1,27 @@
 import flask
 
 from flask import render_template
-from flask import request
+from flask import request,session
 
 app = flask.Flask(__name__)
 
 @app.route('/')
-def index(sport=0):
-    return render_template('index.html',selection = ["Snorkelling","Diving"], sport = sport)
+def index(sport=0,price=0):
+    return render_template('index.html',selection = ["Snorkelling","Diving"], sport = sport, price=price)
 
 @app.route('/price', methods = ['GET','POST'])
-def price(sport=0):
+def price(sport=0,price=0):
     sport = request.form['sports']
-    return render_template('index.html',selection = ["Snorkelling","Diving"], sport = sport)
+    if requst.method == 'GET':
+        price = request.form['price']
+        return render_template('index.html',selection = ["Snorkelling","Diving"], sport = sport, price = price)
+    else:
+    #calculator = PriceCalculating()
+    #calculator.setSport(sport)
+    #price = calculator.calculate()
+        return render_template('index.html',selection = ["Snorkelling","Diving"], sport = sport, price = price)
 
-class PrizeCalculating():
+class PriceCalculating():
     #Initialize as 0, 
     __sport = 0
     
